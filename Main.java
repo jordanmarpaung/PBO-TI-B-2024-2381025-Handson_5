ed180095de28bd597f2d77e10c6ae24024330b9a
Jordan <2381025@unai.edu>
2024-09-29 18:07:03 +0700

    handson4 edit

--- Main.java
+++ Main.java
@@ -1,105 +1,97 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.SortedMap;
 
 public class Main {
     public static String[] todos = new String[10];
-    public static Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner( System.in);
 
     public static void main(String[] args) {
-        showTodoList();
-        addTodoList();[0] = "Belajar";
-        addTodoList();[1] = "Menggambar";
-        addTodoList();[2] = "Berdoa";
-        showTodoList();
-        removeTodoList( number: 2);
-        showTodoList();
-        removeTodoList( number: 1);
+        addTodoList("Belajar");
+        addTodoList("Menggambar");
+        addTodoList("Menulis");
+        addTodoList("Menyetir");
+        System.out.println("After Edit");
+        editTodoList(2, "Menari");
         showTodoList();
     }
-    public static void showTodoList() {
-        System.out.println("TODO LIST");
+    public static void showTodoList(){
+        System.out.println("Todo List");
         for (int i = 0; i < todos.length; i++) {
             String todo = todos[i];
             if (todo != null) {
                 System.out.println((i + 1) + ". " + todo);
             }
         }
-}
+    }
 
     public static void addTodoList(String todo){
-        Boolean isFull = true;
-        for (int i = 0; i < todos.length; i++) {
-            if (todos[i]) == null) {
-                isFull = false;
+
+        resizeIfFull();
+
+
+        //add todo to array that has null element
+        for (int i = 0; i < todos.length; i++){
+            if (todos[i] == null){
+                todos[i] = todo;
                 break;
             }
         }
 
-
-    if (isFull){
-        resizeIfFull();
     }
 
+    private static void resizeIfFull() {
+        Boolean isFUll = true;
+        for (int i = 0; i < todos.length; i++){
+            if (todos[i] == null){
+                isFUll = false;
+                break;
+            }
 
+        }
+
+        if (isFUll){
+            resizeArrayToTwoTimesBigger();
+        }
+    }
     private static void resizeArrayToTwoTimesBigger() {
-        String [] temp = todos;
+        String[] temp = todos;
         todos = new String[todos.length * 2];
-        for(int i = 0; i < temp.length; i++){
+        for (int i = 0; i < temp.length; i++){
             todos[i] = temp[i];
         }
-        }
-
-
-
-
-
-
-
-    public static boolean removeTodoList(Integer number) {
-        if (number <= 0);
-        return false;
-        }
-    if (number - 1 > todos.length - 1){
-        return false;
-        }
-    if (todos[number - 1] == null) {
-        return false;
-
     }
 
-    for (int i = number -1; i < todos.length; i++) {
-        if (i == (todos.length - 1)) {
-            todos(i) = null;
-        }else{
-            todos(i) = todos[i + 1];
+    public static boolean removeTodoList(Integer number){
+        if (isSelectedTodoNotValid(number)) return false;
+
+        for (int i = number- 1; i < todos.length; i++){
+            if (i == (todos.length - 1)) {
+                todos[i] = null;
+            }else {
+                todos[i] = todos[i + 1];
+            }
         }
-        }
-    return true;
+        return true;
     }
 
-    private static boolean isSelectedTodoNotValid
-
-
-
-public static boolean editTodoList(Integer number, String newTodo) {
-    if (isSelectedTodoNotValid(number)) {
+    private static boolean isSelectedTodoNotValid(Integer number) {
+        if (number <= 0){
+            return true;
+        }
+        if (number - 1 > todos.length -1){
+            return true;
+        }
+        if (todos[number -1] == null){
+            return true;
+        }
         return false;
     }
-    todos[number - 1] = newTodo;
-    return true;
+    public static boolean editTodoList(Integer number, String newTodo){
+        if (isSelectedTodoNotValid(number)){
+            return false;
+        }
+        todos[number - 1] = newTodo;
+        return true;
+    }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
